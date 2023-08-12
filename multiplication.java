3b383296de7a85d4d798de293888d6ad7150b85d
omraje Ambure <111331642+OmrajeAmbure@users.noreply.github.com>
2023-08-12 12:14:42 +0530

    This basic Java file

---
+++ multiplication.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class Multiplication {
+	public static void main(String[] args) {
+		int n ,result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr your number :- ");
+		n = sc.nextInt();
+		
+		for (int i = 1; i<=10;i++)
+		{
+		    
+		     System.out.println(+n  +"×"  +i  +"=" +(+n * +i));
+		 }
+	}
+}
