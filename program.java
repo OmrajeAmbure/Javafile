98333ac9a320c369ec991b127deca3e1add72117
omraje Ambure <111331642+OmrajeAmbure@users.noreply.github.com>
2024-06-09 10:54:16 +0530

    how to run program
    this is first program to run on java language.

---
+++ program.java
@@ -0,0 +1,10 @@
+public class program {
+    public static void main(String args[]){
+        System.out.format("%10s %15s %10s\n","sr.no","Name","Marks");
+        System.out.format("%10d %15s %10d\n",1,"omraje",89);
+        System.out.format("%10d %15s %10d\n",1,"omraje",89);
+        System.out.format("%10d %15s %10d\n",1,"omraje",89);
+        System.out.format("%10d %15s %10d\n",1,"omraje",89);
+        System.out.format("%10d %15s %10d\n",1,"omraje",89);
+    }
+}
