ab99b95aa9447f101a60b9834b51fa71fe5496c3
omraje Ambure <111331642+OmrajeAmbure@users.noreply.github.com>
2024-06-11 23:38:56 +0530

    Escape sequence example code
    In this code we learn about escape sequence in java

---
+++ EscapeSequenceExample1.java
@@ -0,0 +1,9 @@
+public class EscapeSequenceExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!\n");       // New line
+        System.out.println("Hello,\tWorld!");        // Tab
+        System.out.println("This is a backslash: \\");// Backslash
+        System.out.println("She said, \"Hello!\"");  // Double quote
+        System.out.println("It\'s a beautiful day!"); // Single quote
+    }
+}
