514ac62f6e1fa072723018084599f446c74468ed
omraje Ambure <111331642+OmrajeAmbure@users.noreply.github.com>
2024-06-14 22:02:27 +0530

    swap the two number
    In, This Program we can crate the two variable . In which we can first variable value can be assign to second variable and second variable value can assign to first variable.

---
+++ swap_number.java
@@ -0,0 +1,15 @@
+public class swap_number {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println("before swapping A :- "+a);
+        System.out.println("before Swapping b :- "+b);
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("After Swapping A :-  "+a);
+        System.out.println("After Swapping B :-  "+b);
+
+    }
+}
