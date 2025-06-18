8bcc2d7a8f2a6e5b846088d4cc935b8feb680eb3
omraje Ambure <111331642+OmrajeAmbure@users.noreply.github.com>
2025-06-18 21:34:44 +0530

    Day1

---
+++ printNumber.java
@@ -0,0 +1,16 @@
+package Leran_the_basics;
+
+import java.util.Scanner;
+
+public class printNumber {
+    public void printNumber(Scanner sc){
+        int input = sc.nextInt();
+        System.out.println(input);
+    }
+
+    public static void main(String[] args) {
+        printNumber printNumber = new printNumber();
+//        printNumber.printNumber();
+//        printNumber.printNumber();
+    }
+}
